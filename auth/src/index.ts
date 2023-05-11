f03650ba757bd2cdd6524124c4369d497a098d9f
Warren <warrengamedev@gmail.com>
2023-05-11 10:50:21 +0100

    pull code from src and clean up

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,11 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res )=>{
-    res.send("hi there");
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
 });
 
-
-app.listen(3000, ()=> {
-    console.log("AUTH listening on 3000 !!!!");
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
 });
