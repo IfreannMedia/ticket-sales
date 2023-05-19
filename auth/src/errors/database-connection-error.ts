5e815138e3ec8de7a38b6d2ed4239d0c18884d96
Warren <warrengamedev@gmail.com>
2023-05-19 11:41:13 +0100

    updated custom error handling with abstract class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,15 @@
 export class DatabaseConnectionError extends Error {
-
+    statusCode = 500;
     reason = 'error connecting to database';
     constructor() {
         super();
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    public serializeErrors() {
+        return [
+            { message: this.reason }
+        ]
     }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,21 @@
 import { ValidationError } from "express-validator";
+import { CustomError } from "./custom-error";
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+    statusCode = 400;
 
-    constructor(public errors: ValidationError[]){
-        super();
+    constructor(public errors: ValidationError[]) {
+        super('Invalid request parameters');
 
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    public serializeErrors() {
+        return this.errors.map((error: ValidationError) => {
+            if (error.type === 'field') {
+                return { message: error.msg, field: error.path };
+            }
+            return { message: error.msg };
+        });
     }
 }
