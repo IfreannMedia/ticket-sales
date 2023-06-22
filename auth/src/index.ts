509275972c189406248fb5c5931e20ec6f552128
Warren <warrengamedev@gmail.com>
2023-06-22 16:23:53 +0100

    added JWT token in session cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,17 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+)
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
@@ -23,6 +31,10 @@ app.all('*', async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if(!process.env.JWT_KEY){
+    throw new Error("JWT key is not defined");
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('connected to MONGODB');
@@ -32,7 +44,7 @@ const start = async () => {
 
   app.listen(3000, () => {
     console.log("Listening on port 3000!!!!!!!!");
-  });  
+  });
 };
 
 start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -27,7 +28,14 @@ router.post("/api/users/signup", [
 
     const user = User.build({ email, password });
     await user.save();
-    
+
+    // generate jwt
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+    // store on session
+    req.session = { jwt: userJwt };
     res.status(201).send(user);
 });
 
